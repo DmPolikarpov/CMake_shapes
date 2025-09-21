716ab8d9ca0991880a8d313cd745a0661d014a6f
Dmitrii Polikarpov <dpolikarpov17@gmail.com>
2025-09-21 23:04:42 +0300

    task 10.2

--- CMake_shapes.cpp
+++ CMake_shapes.cpp
@@ -1,9 +1,74 @@
 ﻿#include <iostream>
+#include <Windows.h>
+
+#include "./shape/shape.h"
+
+#include "./triangle/triangle.h"
+#include "./rightTriangle/rightTriangle.h"
+#include "./isoscelesTriangle/isoscelesTriangle.h"
+#include "./equilateralTriangle/equilateralTriangle.h"
+
+#include "./square/square.h"
+#include "./parallelogram/parallelogram.h"
+#include "./rhomb/rhomb.h"
+#include "./quadrangle/quadrangle.h"
+#include "./rectangle/rectangle.h"
+
+// COMMON FUNCTIONS
+void print_info(Shape* shape);
 
 
 int main()
 {
-	
+    setlocale(LC_ALL, "Russian");
 
-	return EXIT_SUCCESS;
+    // TRIANGLES
+
+    Triangle triangle{ 10, 20, 30, 40, 50, 60 };
+    RightTriangle rightTriangle{ 10, 20, 30, 40, 50 };
+    IsoscelesTriangle isoscelesTriangle{ 10, 20, 40, 50 };
+    EquilateralTriangle equilateralTriangle{ 10 };
+
+    // QUADRANGLES
+
+    Quadrangle quadrangle{ 10, 20, 30, 40, 50, 60, 70, 80 };
+    Rectangle1 rectangle{ 10, 20 };
+    Square square{ 10 };
+    Parallelogram parallelogram{ 10, 20, 50, 60 };
+    Rhomb rhomb{ 20, 50, 60 };
+
+    // TRIANGLES DATA
+
+    std::cout << "Треугольник: " << std::endl;
+    print_info(&triangle);
+    std::cout << "Прямоугольный треугольник: " << std::endl;
+    print_info(&rightTriangle);
+    std::cout << "Равнобедренный треугольник: " << std::endl;
+    print_info(&isoscelesTriangle);
+    std::cout << "Равносторонний треугольник: " << std::endl;
+    print_info(&equilateralTriangle);
+
+    std::cout << std::endl;
+
+    // QUADRANGLE DATA
+
+    std::cout << "Четырёхугольник: " << std::endl;
+    print_info(&quadrangle);
+    std::cout << "Прямоугольник: " << std::endl;
+    print_info(&rectangle);
+    std::cout << "Квадрат: " << std::endl;
+    print_info(&square);
+    std::cout << "параллелограмм: " << std::endl;
+    print_info(&parallelogram);
+    std::cout << "Ромб: " << std::endl;
+    print_info(&rhomb);
+
+
+    return EXIT_SUCCESS;
 }
+
+void print_info(Shape* shape)
+{
+    shape->printSides();
+    shape->printAngles();
+};
